5dc5428ea4e8638850f9aeec7ef2dbd76ec02e78
Jerry777777 <491818175@qq.com>
2020-09-07 13:51:12 +0800

    [xiawenhao] feat: add votecount and refactor main

--- src/com/twu/user/User.java
+++ src/com/twu/user/User.java
@@ -2,10 +2,11 @@ package com.twu.user;
 
 public class User {
     private String name;
+    private int voteCount;
 
     public User(String name) {
-
         this.name = name;
+        this.voteCount=10;
     }
 
     public String getName() {
@@ -16,5 +17,13 @@ public class User {
     public void setName(String name) {
 
         this.name = name;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
     }
 }
