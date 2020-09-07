914c6da370c5bc205ebcf8c6ff295882c5c991f3
Jerry777777 <491818175@qq.com>
2020-09-07 10:10:52 +0800

    [xiawenhao] feat: add entities

---
+++ src/com/twu/HotSearchManager.java
@@ -0,0 +1,52 @@
+package com.twu;
+
+import java.util.List;
+
+public class HotSearchManager {
+    private List<HotSearch> hotSearchList;
+    private List<Integer> hotSearchPrice;
+
+    public HotSearchManager(List<HotSearch> hotSearchList, List<Integer> hotSearchPrice) {
+        this.hotSearchList = hotSearchList;
+        this.hotSearchPrice = hotSearchPrice;
+    }
+
+    public List<HotSearch> getHotSearchList() {
+        return hotSearchList;
+    }
+
+    public void setHotSearchList(List<HotSearch> hotSearchList) {
+        this.hotSearchList = hotSearchList;
+    }
+
+    public List<Integer> getHotSearchPrice() {
+        return hotSearchPrice;
+    }
+
+    public void setHotSearchPrice(List<Integer> hotSearchPrice) {
+        this.hotSearchPrice = hotSearchPrice;
+    }
+
+    public void printHotSearchList() {
+        this.hotSearchList.forEach(System.out::println);
+    }
+
+    public void addHotSearch(HotSearch hotSearch) {
+        this.hotSearchList.add(hotSearch);
+    }
+
+    public void addSuperHotSearch(HotSearch hotSearch) {
+        this.hotSearchList.add(hotSearch);
+    }
+
+    public void voteForHotSearch(String content, int vote) {
+        for (HotSearch hotSearch : this.hotSearchList) {
+            if (hotSearch.getContent().equals(content)) {
+                hotSearch.setVote(hotSearch.getVote() + vote);
+                break;
+            } else {
+                return;
+            }
+        }
+    }
+}
